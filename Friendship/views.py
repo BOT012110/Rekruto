e82772d3b3fc50d0b21917c93e892336bb230dce
= <=>
2022-03-15 01:23:44 +0300

    add objects from models to the nice.html

--- Friendship/views.py
+++ Friendship/views.py
@@ -1,6 +1,10 @@
+from email import message
+from unicodedata import name
 from django.shortcuts import render
+from .models import StartFriendShip
 
 def site(request):
-    return render(request, 'Friendship/nice.html', {})  
+    f_text = StartFriendShip.objects.all()
+    return render(request, 'Friendship/nice.html', {'start_f': f_text})  
 
 # Create your views here.
